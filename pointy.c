e0f71d8e7f3801e3fcf4efd6d47badd02ca95475
horazio <hfusco@stuy.edu>
2018-09-16 15:07:21 -0400

    hex is very useful

---
+++ pointy.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+  unsigned int num = 1030500302;
+  char * p2 = &num;
+  //int * p1  = &num;
+  //unsigned int num2 = * p1; 
+  // p1++;
+  printf(" %x \n", num);
+  printf(" %d \n ", num);
+  
+
+  
+  printf(" %hhx \n", * p2);
+
+  p2++;
+
+  printf(" %hhx \n", *p2);
+
+  p2++;
+
+  printf(" %hxx \n", *p2);
+
+  p2++;
+
+  printf(" %hxx \n", *p2);
+
+  p2--;
+  p2--;
+  * p2 += 16;
+  
+  p2--;
+  
+  * p2 += 1;
+  printf(" %x \n", num);
+  printf(" %d \n ", num);
+
+  return 0;
+}
